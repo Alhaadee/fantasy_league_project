b07cb818eb340dea21b0d58cafc2914714155eb6
Alhaadee <alhaadee91@gmail.com>
2022-10-19 15:35:32 +0100

    adds templates

---
+++ fantasy-league-api/src/main/java/com/example/fullstackproject/fantasyleagueapi/FantasyLeagueApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.fullstackproject.fantasyleagueapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FantasyLeagueApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FantasyLeagueApiApplication.class, args);
+	}
+
+}
---
+++ fantasy-league-api/src/test/java/com/example/fullstackproject/fantasyleagueapi/FantasyLeagueApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fullstackproject.fantasyleagueapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FantasyLeagueApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
