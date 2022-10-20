ad79bc9a29fb7fd4bf25ab87057f867e7e8178b0
Tariq-Hennache <tariq.hennache@gmail.com>
2022-10-20 10:55:00 +0100

    controller mappings added

--- backend/fantasy-league-api/src/main/java/com/example/fullstackproject/fantasyleagueapi/controllers/PlayerController.java
+++ backend/fantasy-league-api/src/main/java/com/example/fullstackproject/fantasyleagueapi/controllers/PlayerController.java
@@ -1,4 +1,25 @@
 package com.example.fullstackproject.fantasyleagueapi.controllers;
 
+import com.example.fullstackproject.fantasyleagueapi.models.Player;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/players")
 public class PlayerController {
+
+
+    @PostMapping
+    public ResponseEntity<Player> addNewPlayer(@RequestBody Player player){}
+
+    @GetMapping
+    public ResponseEntity<List<Player>> getAllPlayers(){}
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Player> getPlayerById(@PathVariable long id){}
+
+
+
 }
