2962df0afae836ae24a39911e7bbb2402801185d
tio982 <t6babayemi@gmail.com>
2022-10-20 10:26:30 +0100

    added to player model and repository

--- backend/fantasy-league-api/src/main/java/com/example/fullstackproject/fantasyleagueapi/repositories/PlayerRepository.java
+++ backend/fantasy-league-api/src/main/java/com/example/fullstackproject/fantasyleagueapi/repositories/PlayerRepository.java
@@ -1,4 +1,8 @@
 package com.example.fullstackproject.fantasyleagueapi.repositories;
 
-public class PlayerRepository {
+import com.example.fullstackproject.fantasyleagueapi.models.Player;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public class PlayerRepository extends JpaRepository <Player, Long>  {
+
 }
