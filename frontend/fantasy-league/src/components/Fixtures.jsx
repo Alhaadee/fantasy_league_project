83f326021756117cbd83509b6bfea8af07dbe345
Alhaadee <alhaadee91@gmail.com>
2022-10-24 14:12:33 +0100

    refactored fixtures code

--- frontend/fantasy-league/src/components/Fixtures.jsx
+++ frontend/fantasy-league/src/components/Fixtures.jsx
@@ -4,55 +4,36 @@ export default function Fixtures({fixtures, data, teamNames, playerNames, loadin
   
   
 
+  const normalGoalIndex = 0
+  const ownGoalIndex = 2
 
-  // const upcomingGames = fixtures.filter((fixture)=>fixture.event === 12)
-  // const homeGoalsScored = upcomingGames.map(fixture=>{
-  //   if(!loading && fixture.stats.length !== 0 && fixture.stats[0].h.length !== 0){
-  //     return(
-  //     fixture.stats[0].h.map(playerScored => {
-  //       return (<p>{playerNames[playerScored.element]} : {playerScored.value}</p>)
-  //     })
-  //     )
-  //   } 
-    
-  // })
-
-const homeGoalsScored = (fixture) => {
-  if(!loading && fixture.stats.length !== 0 && fixture.stats[0].h.length !== 0){
+const awayGoalsScored = (fixture,goalType) => {
+  if(!loading && fixture.stats.length !== 0 && fixture.stats[goalType].a.length !== 0){
     return(
-    fixture.stats[0].h.map((playerScored,index) => {
+    fixture.stats[goalType].a.map((playerScored,index) => {
       return (<li key={fixture.id+index}>{playerNames[playerScored.element]} : {playerScored.value}</li>)
     })
     )
   }
 }
-const homeOwnGoals = (fixture) => {
-  if(!loading && fixture.stats.length !== 0 && fixture.stats[2].h.length !== 0){
-    return(
-    fixture.stats[2].h.map((playerScored,index) => {
-      return (<li key={fixture.id+index}>{playerNames[playerScored.element]} : {playerScored.value}(OG)</li>)
-    })
-    )
-  }
-}
 
-const awayOwnGoals = (fixture) => {
-  if(!loading && fixture.stats.length !== 0 && fixture.stats[2].a.length !== 0){
+const homeGoalsScored = (fixture,goalType) => {
+  if(!loading && fixture.stats.length !== 0 && fixture.stats[goalType].h.length !== 0){
     return(
-    fixture.stats[2].a.map((playerScored,index) => {
-      return (<li key={fixture.id+index}>{playerNames[playerScored.element]} : {playerScored.value}(OG)</li>)
-    })
-    )
-  }
-}
-
-const awayGoalsScored = (fixture) => {
-  if(!loading && fixture.stats.length !== 0 && fixture.stats[0].a.length !== 0){
-    return(
-    fixture.stats[0].a.map((playerScored,index) => {
+    fixture.stats[goalType].h.map((playerScored,index) => {
       return (<li key={fixture.id+index}>{playerNames[playerScored.element]} : {playerScored.value}</li>)
     })
     )
+  }
+}
+
+const displayScoreboard = (fixture) => {
+  if(fixtures.started & !fixture.finished){
+    return <p>{fixture.team_h_score}:{fixture.team_a_score} (LIVE)</p>
+  } else if (fixture.finished){
+    return <p>{fixture.team_h_score}:{fixture.team_a_score}</p>
+  } else {
+    return <p>Kick Off time: {fixture.kickoff_time}</p>
   }
 }
 
@@ -60,25 +41,21 @@ const awayGoalsScored = (fixture) => {
  
   
   const fixturesList = fixtures.map((fixture)=>{
-    if (fixture.event === 13){
+    if (fixture.event === 12){
       return(
         <div key={fixture.id} className='fixture_card'>
-        <h3 >{teamNames[fixture.team_h]} Vs {teamNames[fixture.team_a]}</h3>
-        {fixture.started  & !fixture.finished ?  <p>{fixture.team_h_score}:{fixture.team_a_score} (LIVE)</p> : <></>}
-        {fixture.finished ?  <p>{fixture.team_h_score}:{fixture.team_a_score}</p> : <></>}
-        {!fixture.started ?  <p>Kick Off time: {fixture.kickoff_time}</p>: <></>}
-        <div className='players-scored'>
-        <ul className='left'>
-        {homeGoalsScored(fixture)}
-        {awayOwnGoals(fixture)}
-        </ul>
-        <ul className='right'>
-        {awayGoalsScored(fixture)}
-        {homeOwnGoals(fixture)}
-        </ul>
-        </div>
-       
-       
+          <h3 >{teamNames[fixture.team_h]} Vs {teamNames[fixture.team_a]}</h3>
+          {displayScoreboard(fixture)}
+          <div className='players-scored'>
+            <ul className='left'>
+              {homeGoalsScored(fixture,normalGoalIndex)}
+              {homeGoalsScored(fixture,ownGoalIndex)}
+            </ul>
+            <ul className='right'>
+              {awayGoalsScored(fixture,normalGoalIndex)}
+              {awayGoalsScored(fixture,ownGoalIndex)}
+            </ul>
+          </div>
         </div>
         )
     }
